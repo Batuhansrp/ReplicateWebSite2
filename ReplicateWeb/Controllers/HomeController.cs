32e070633b7f94cd6ba500dcf6b36c206e7a4887
Batuhansrp <68540266+Batuhansrp@users.noreply.github.com>
2021-01-08 09:41:16 +0300

    Replicate

---
+++ ReplicateWeb/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReplicateWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DemoSayfasi()
+        {
+            return View();
+        }
+        public ActionResult TrainingSayfasi()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ReplicateWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReplicateWeb.MvcApplication" Language="C#" %>
